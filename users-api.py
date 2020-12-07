7a9dcd227e3cccc90f3cbdfe88fe336a5c11f10d
Ubuntu <ubuntu@ip-172-31-13-218.us-west-1.compute.internal>
2020-12-07 23:22:25 +0000

    Added a user api. It gets the users from the database and returns it as a json string. Assumes that there are users in the database.

---
+++ users-api.py
@@ -0,0 +1,29 @@
+"""
+Joanna Lowry
+
+Users API
+"""
+
+from flask import Flask, request
+from flask_restful import Resource, Api
+import mysql.connector
+import json
+
+cnx = mysql.connector.connect(user='admin', password='capstone', host='127.0.0.1', database='pellego_database')
+app = Flask(__name__)
+api = Api(app)
+
+
+class GetUser(Resource):
+    def get(self):
+        query = "select * from Users"
+        cursor = cnx.cursor(dictionary=True)
+        		
+        cursor.execute(query)
+        result = cursor.fetchall()
+        cursor.close()
+        return json.loads(json.dumps(result))
+
+api.add_resource(GetUser, "/getuser")
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port='5001')
