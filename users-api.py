2b2949fe6970f5c911d1e911009a802e3cd5b192
Arturo <arturo.lara541@gmail.com>
2021-02-17 20:39:07 -0700

    updated db

--- users-api.py
+++ users-api.py
@@ -10,7 +10,7 @@ from flask_restful import Resource, Api, reqparse
 import mysql.connector
 import json
 
-cnx = mysql.connector.connect(user='admin', password='capstone', host='127.0.0.1', database='pellego_database')
+cnx = mysql.connector.connect(user='admin', password='capstone', host='pellego-db.cdkdcwucys6e.us-west-2.rds.amazonaws.com', database='pellego_database')
 app = Flask(__name__)
 api = Api(app)
 
