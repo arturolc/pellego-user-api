9a5c388140351e3e44592728cab164f52843ebcb
Joanna_Lowry <joanna.lowrym@gmail.com>
2021-03-09 21:32:57 -0700

    Cleaned up some code

--- users-api.py
+++ users-api.py
@@ -49,16 +49,11 @@ class AddUser(Resource):
         except:
             cnx = mysql.connector.connect(user='admin', password='capstone', host='pellego-db.cdkdcwucys6e.us-west-2.rds.amazonaws.com', database='pellego_database')
             
-        #parser.add_argument('name', type=str)
-        #parser.add_argument('email', type=str)
-        #args = parser.parse_args()
         json_data = request.get_json(force=True)
         query = ("INSERT INTO Users (Email, Name) VALUES(%s, %s)")
         cursor = cnx.cursor(dictionary=True)
-        #data = (args['email'], args['name'])
         args = json_data[0]
         data = (args['email'], args['name'])
-        #print(args)
         print(data)        
         try: 
             cursor.execute(query, data)
@@ -72,8 +67,7 @@ class AddUser(Resource):
             return [{'status' : 'Something went wrong'}]
         
 
-#Add a resource for getting a user intro status
-#Gets and returns the status of all the learning modules of a particular user
+#Gets and returns the status of all the learning modules intros of a particular user
 class GetIntroStatus(Resource):
     def get(self, email):
         # do a simple query to check if MySQL connection is open
@@ -94,10 +88,9 @@ class GetIntroStatus(Resource):
         cursor.close()
         return json.loads(json.dumps(result))
 
-#Add a resource for getting a user LM_submodule status
-#Is a little more complicated then I thought
+#Gets the all the LM_submodule status's for one user
 class GetSubmoduleStatus(Resource):
-    def get(self, email):
+    def get(self, module, email):
         # do a simple query to check if MySQL connection is open
         try:
             cursor = cnx.cursor(dictionary=True)
@@ -118,7 +111,10 @@ class GetSubmoduleStatus(Resource):
 
 
 api.add_resource(GetUser, "/user/<string:email>")
-api.add_resource(AddUser, '/user/add')
+api.add_resource(AddUser, "/user/add")
+api.add_resource(GetSubmoduleStatus, "/user/submodule/<string:email>")
+api.add_resource(GetIntroStatus, "/user/intro/<string:email>")
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port='5000')
+    app.run(host="0.0.0.0", port='5001')
