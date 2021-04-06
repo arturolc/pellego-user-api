463d6393f77275f36b925965efb8ca63fc5501f3
Arturo <arturo.lara541@gmail.com>
2021-04-05 19:07:27 -0600

    Cleaned up api for data collection

--- users-api.py
+++ users-api.py
@@ -1,79 +1,75 @@
 """
-Joanna Lowry
+Joanna Lowry & Arturo Lara
 
 Users API
 """
 
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
-
-import mysql.connector
+from jose import jwk, jwt
+from jose.utils import base64url_decode
 import json
+import time
+import urllib.request
+import mysql.connector
 
 cnx = mysql.connector.connect(user='admin', password='capstone', host='pellego-db.cdkdcwucys6e.us-west-2.rds.amazonaws.com', database='pellego_database')
 app = Flask(__name__)
 api = Api(app)
-
-parser = reqparse.RequestParser()
-
-#Gets the User first and last name given an email
-class GetUser(Resource):
-    def get(self, email):
-        # do a simple query to check if MySQL connection is open
-        try:
-            cursor = cnx.cursor(dictionary=True)
-            cursor.execute("Select 1")
-            cursor.fetchall()
-            cursor.close()
-        except:
-            cnx = mysql.connector.connect(user='admin', password='capstone', host='pellego-db.cdkdcwucys6e.us-west-2.rds.amazonaws.com', database='pellego_database')
-
-        query = ("select Name from Users where Email=%s")
-        cursor = cnx.cursor(dictionary=True)
-        		
-        cursor.execute(query, (email,))
-        
-        result = cursor.fetchall()
-        cursor.close()
-        return json.loads(json.dumps(result))
+region = 'us-west-2'
+userpool_id = 'us-west-2_AdDJsuC6f'
+app_client_id = 'o4uoksbrsfa78eo644tpf20um'
+keys_url = 'https://cognito-idp.{0}.amazonaws.com/{1}/.well-known/jwks.json'.format(region, userpool_id)
 
 
-class AddUser(Resource):
-    def post(self):
-        # do a simple query to check if MySQL connection is open
-        try:
-            cursor = cnx.cursor(dictionary=True)
-            cursor.execute("Select 1")
-            cursor.fetchall()
-            cursor.close()
-        except:
-            cnx = mysql.connector.connect(user='admin', password='capstone', host='pellego-db.cdkdcwucys6e.us-west-2.rds.amazonaws.com', database='pellego_database')
-            
-        #parser.add_argument('name', type=str)
-        #parser.add_argument('email', type=str)
-        #args = parser.parse_args()
-        json_data = request.get_json(force=True)
-        query = ("INSERT INTO Users (Email, Name) VALUES(%s, %s)")
-        cursor = cnx.cursor(dictionary=True)
-        #data = (args['email'], args['name'])
-        args = json_data[0]
-        data = (args['email'], args['name'])
-        #print(args)
-        print(data)        
-        try: 
-            cursor.execute(query, data)
+# instead of re-downloading the public keys every time
+# we download them only on cold start
+# https://aws.amazon.com/blogs/compute/container-reuse-in-lambda/
+with urllib.request.urlopen(keys_url) as f:
+  response = f.read()
+keys = json.loads(response.decode('utf-8'))['keys']
 
-            cnx.commit()
-            cursor.close()
-            return {'status' : 'User Added'}
-        except:
-            cnx.rollback()
-            cnx.close()
-            return {'status' : 'Something went wrong'}
-        
+def verifyToken(token):
+    # get the kid from the headers prior to verification
+    headers = jwt.get_unverified_headers(token)
+    kid = headers['kid']
+    # search for the kid in the downloaded public keys
+    key_index = -1
+    for i in range(len(keys)):
+        if kid == keys[i]['kid']:
+            key_index = i
+            break
+    if key_index == -1:
+        print('Public key not found in jwks.json')
+        return False
+    # construct the public key
+    public_key = jwk.construct(keys[key_index])
+    # get the last two sections of the token,
+    # message and signature (encoded in base64)
+    message, encoded_signature = str(token).rsplit('.', 1)
+    # decode the signature
+    decoded_signature = base64url_decode(encoded_signature.encode('utf-8'))
+    # verify the signature
+    if not public_key.verify(message.encode("utf8"), decoded_signature):
+        print('Signature verification failed')
+        return False
+    print('Signature successfully verified')
+    # since we passed the verification, we can now safely
+    # use the unverified claims
+    claims = jwt.get_unverified_claims(token)
+    # additionally we can verify the token expiration
+    if time.time() > claims['exp']:
+        print('Token is expired')
+        return False
+    # and the Audience  (use claims['client_id'] if verifying an access token)
+    if claims['aud'] != app_client_id:
+        print('Token was not issued for this audience')
+        return False
+    # now we can use the claims
+    print(claims)
+    return claims
 
-api.add_resource(GetUser, "/user/<string:email>")
-api.add_resource(AddUser, '/', '/user/add')
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port='5000')
