f4087f92aed840fa38fbf15fc31a407d2b5d2a3f
u1215936 <u1215936@utah.edu>
2021-04-07 18:32:13 -0600

    fixed column count error

--- users-api.py
+++ users-api.py
@@ -85,7 +85,7 @@ class QuizResults(Resource):
         cursor.close()
 
         cursor = cnx.cursor(dictionary=True)
-        query = ("insert ignore into ProgressCompleted (UID, SMID) values (%s, %s, %s)")
+        query = ("insert ignore into ProgressCompleted (UID, MID, SMID) values (%s, %s, %s)")
         cursor.execute(query, (userID, module_id, submodule_id,))
         cursor.close()
 
