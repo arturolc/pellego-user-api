565b747360e34a95f6defad8ff7277628c60275f
u1215936 <u1215936@utah.edu>
2021-04-12 01:23:10 -0600

    Attempted to convert the decimals returned to integers

--- users-api.py
+++ users-api.py
@@ -199,7 +199,8 @@ class TodayProgressValues(Resource):
         cursor.close()
 
         cursor = cnx.cursor(dictionary=True)
-        query = ("select round(AVG(WordsRead),0) as WordsRead, round(AVG(WPM),0) as WPM from User_Word_Values where UID = %s and Recorded = %s")
+        query = ("select round(cast(AVG(WordsRead) as UNSIGNED),0) as WordsRead, round(cast(AVG(WPM) as UNSIGNED),0) as WPM from User_Word_Values where UID = %s and Recorded = %s")
+
         cursor.execute(query, (userID, date.today(),))
         result = cursor.fetchall()
         cursor.close()
