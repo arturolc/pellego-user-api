c3232161a400fe39543daa0af901b9867194ad51
u1215936 <u1215936@utah.edu>
2021-04-11 22:32:00 -0600

    Trying to fix decimal conversion issue

--- users-api.py
+++ users-api.py
@@ -9,6 +9,7 @@ from flask_restful import Resource, Api, reqparse
 from jose import jwk, jwt
 from jose.utils import base64url_decode
 from datetime import date
+from decimal import Decimal
 import json
 import time
 import urllib.request
