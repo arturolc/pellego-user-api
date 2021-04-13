925883594e39d515342239dfce97cb374c0d59ec
u1215936 <u1215936@utah.edu>
2021-04-12 18:12:33 -0600

    Debug mode

--- users-api.py
+++ users-api.py
@@ -197,11 +197,11 @@ class TodayProgressValues(Resource):
         cursor.execute(("select UID from Users where Email = %s"), (res['email'],))
         userID = int(cursor.fetchall()[0]['UID'])
         cursor.close()
-
+        python3
         cursor = cnx.cursor(dictionary=True)
         query = ("select round(cast(AVG(WordsRead) as UNSIGNED),0) as WordsRead, round(cast(AVG(WPM) as UNSIGNED),0) as WPM from User_Word_Values where UID = %s and Recorded = %s")
 
-        cursor.execute(query, (userID, date.today(),))
+        cursor.execute(query, (userID, date.today() - timedelta(hours=6),))
         result = cursor.fetchone()
         print(result)
         #res = {result}
