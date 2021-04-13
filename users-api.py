5e1eba34b1d4376dd004729f7cba065267febcc5
u1215936 <u1215936@utah.edu>
2021-04-13 06:08:51 -0600

    Working on Progress Reports

--- users-api.py
+++ users-api.py
@@ -207,7 +207,7 @@ class ProgressValues(Resource):
 
         for item in range(1,13):
             cursor = cnx.cursor(dictionary=True)
-            query = ("select WordsRead, WPM, Recorded from User_Word_Values where UID = %s and Recorded between date_sub(now(), Interval %s month) and now()")
+            query = ("select round(cast(avg(WordsRead) as UNSIGNED), 0) as WordsRead, round(cast(avg(WPM) as UNSIGNED), 0) as WPM, Recorded from User_Word_Values where UID = %s and Recorded between date_sub(now(), Interval %s month) and now()")
             cursor.execute(query, (userID, item))
             result = cursor.fetchall()
             cursor.close()
